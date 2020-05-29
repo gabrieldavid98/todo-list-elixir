dbb9d9a1d5763d1aa80456a7a9e8b2da2495321e
Gabriel Mendoza <gabrieldavid_98@hotmail.com>
2020-05-28 21:42:33 -0500

    TodoList.CsvImporter imprived

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -53,24 +53,46 @@ defmodule TodoList do
 end
 
 defmodule TodoList.CsvImporter do
-	def import(path) do
-		File.stream!(path)
-		|> Stream.map(&String.replace(&1, "\n", ""))
-		|> Stream.map(&String.split(&1, ","))
-		|> Stream.map(fn data ->
-			date = List.first(data)
-			title = List.last(data)
-
-			[year, month, day] = date
-				|> String.split("/")
-				|> Stream.map(&String.to_integer/1)
-				|> Enum.map(&(&1))
-
-			{{year, month, day}, title}
-		end)
-		|> Enum.map(fn data ->
-			%{date: elem(data, 0), title: elem(data, 1)}
-		end)
+	def import(file_name) do
+		file_name
+		|> read_lines()
+		|> create_entries()
 		|> TodoList.new()
+	end
+
+	defp read_lines(file_name) do
+		file_name
+		|> File.stream!()
+		|> Stream.map(&String.replace(&1, "\n", ""))
+	end
+
+	defp create_entries(lines) do
+		lines
+		|> Stream.map(&extract_fields/1)
+		|> Stream.map(&create_entry/1)
+	end
+
+	defp extract_fields(line) do
+		line
+		|> String.split(",")
+		|> convert_date()
+	end
+
+	defp convert_date([date_string, title]) do
+		{parse_date(date_string), title}
+	end
+
+	defp parse_date(date_string) do
+		[year, month, day] =
+			date_string
+			|> String.split("/")
+			|> Enum.map(&String.to_integer/1)
+
+		{:ok, date} = Date.new(year, month, day)
+		date
+	end
+
+	defp create_entry({date, title}) do
+		%{date: date, title: title}
 	end
 end
