97c13f7eade88f61f41f0ba2d0b5f0a6d977a3cd
Gabriel David <gabrieldavid_98@hotmail.com>
2020-06-11 19:30:37 -0500

    link process

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -1,18 +1,19 @@
 defmodule Todo.Cache do
 	use GenServer
 
-	def start do
-		GenServer.start(__MODULE__, nil)
+	def start_link(_) do
+		IO.puts("Starting to-do cache")
+		GenServer.start_link(__MODULE__, nil, name: __MODULE__)
 	end
 
 	@impl GenServer
 	def init(_) do
-		Todo.DataBase.start()
+		Todo.DataBase.start_link(nil)
 		{:ok, %{}}
 	end
 
-	def server_process(cache_pid, todo_list_name) do
-		GenServer.call(cache_pid, {:server_process, todo_list_name})
+	def server_process(todo_list_name) do
+		GenServer.call(__MODULE__, {:server_process, todo_list_name})
 	end
 
 	@impl GenServer
@@ -21,7 +22,7 @@ defmodule Todo.Cache do
 			{:ok, todo_server} ->
 				{:reply, todo_server, todo_servers}
 			:error ->
-				{:ok, new_server} = Todo.Server.start(todo_list_name)
+				{:ok, new_server} = Todo.Server.start_link(todo_list_name)
 
 				{
 					:reply, 
